3b49298649337b829a79da3cf3cb6e01c8d41317
pedror13 <pedrohenrique.rosemberg.phsr@gmail.com>
2023-04-06 13:51:24 -0300

    Requisito 24: Implementa logica do fetch

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,38 +1,43 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
-// export async function fetchRecipe(id, tipoReceita) {
-//   console.log(tipoReceita, id);
-//   if (tipoReceita === 'meal') {
-//     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-//     return data;
-//   }
-
-//   if (tipoReceita === 'drink') {
-//     const data = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-//     return data;
-//   }
-// }
-
-function RecipeDetails(props) {
+function RecipeDetails() {
   const { id } = useParams();
-  const { history } = props;
-  // const [receita, setReceita] = useState();
+  const history = useHistory();
+  const { pathname } = history.location;
+  const [receita, setReceita] = useState();
+  const [drinkRecomendados, setDrinkRecomendados] = useState([]);
+  const [comidasRecomendadas, setComidasRecomendadas] = useState([]);
+  const limiteDeReceitas = 6;
   // // const [tipoReceita, setTipo] = useState('');
   console.log(history);
-
-  // console.log(props);
-  // if (history.location.pathname.includes('/meals')) {
-  //   const tipoReceita = 'meal';
-  // }
-  // if (history.location.pathname.includes('/drinks')) {
-  //   const tipoReceita = 'drink';
-  // }
-  // // console.log(tipoReceita);
-  // useEffect(() => {
-  //   fetchRecipe(id, tipoReceita);
-  // }, []);
-  // console.log(receita);
+  useEffect(() => {
+    if (pathname.includes('meals')) {
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setReceita(data.meals[0]);
+        });
+      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+        .then((response) => response.json())
+        .then((data) => {
+          setDrinkRecomendados(data.drinks.slice(0, limiteDeReceitas));
+        });
+    }
+    if (pathname.includes('drinks')) {
+      console.log('Passei');
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setReceita(data.drinks[0]);
+        });
+      fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+        .then((response) => response.json())
+        .then((data) => {
+          setComidasRecomendadas(data.meals.slice(0, limiteDeReceitas));
+        });
+    }
+  }, [pathname, id]);
   return (
     <p>
       olar sou uma receita de id
