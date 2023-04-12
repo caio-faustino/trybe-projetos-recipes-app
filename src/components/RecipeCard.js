a47081182f7950c70c04f9ef2f24c9a7407750bd
Thiago <tm.thiagomorais@gmail.com>
2023-04-12 16:16:38 -0300

    Requisitos 44 a 49 - sem os testes

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,21 +1,73 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import PropTypes, { string } from 'prop-types';
+import clipboardCopy from 'clipboard-copy';
+import { Link } from 'react-router-dom';
+
+import icon from '../images/shareIcon.svg';
 
 function RecipeCard(props) {
-  const { name, image, index } = props;
+  const [linkCopied, setLinkCopied] = useState('');
+  const handleClick = (type, id) => {
+    clipboardCopy(`http://localhost:3000/${type}s/${id}`);
+    setLinkCopied('Link copied!');
+  };
+  const { index, image, name, categoria, date, tags, type, id } = props;
   // console.log(index);
+
   return (
     <div data-testid={ `${index}-recipe-card` }>
-      <h1 data-testid={ `${index}-card-name` }>{ name }</h1>
-      <img
-        data-testid={ `${index}-card-img` }
-        src={ image }
-        alt={ name }
-      />
+      <Link
+        to={ `/${type}s/${id}` }
+      >
+        <h1 data-testid={ `${index}-horizontal-name` }>{ name }</h1>
+      </Link>
+      <p data-testid={ `${index}-horizontal-top-text` }>{ categoria }</p>
+      <Link
+        to={ `/${type}s/${id}` }
+      >
+        <img
+          src={ image }
+          alt={ name }
+          data-testid={ `${index}-horizontal-image` }
+          width="300px"
+        />
+      </Link>
+      <p data-testid={ `${index}-horizontal-done-date` }>{ date }</p>
+      <p
+        data-testid="link-copied"
+      >
+        { linkCopied }
+      </p>
+      <button
+        onClick={ () => handleClick(type, id) }
+      >
+        <img
+          data-testid={ `${index}-horizontal-share-btn` }
+          alt="share button"
+          src={ icon }
+        />
+      </button>
+      {
+        tags && tags.map((tag, i) => (
+          <p
+            key={ `${tag}-${i}` }
+            data-testid={ `${index}-${tag}-horizontal-tag` }
+          >
+            {tag}
+          </p>
+        ))
+      }
     </div>
   );
 }
-
-RecipeCard.propTypes = {}.isRequired;
-
+RecipeCard.propTypes = {
+  index: PropTypes.number,
+  image: PropTypes.string,
+  name: PropTypes.string,
+  categoria: PropTypes.string,
+  date: PropTypes.string,
+  tags: PropTypes.arrayOf(string),
+  id: PropTypes.string,
+  type: PropTypes.string,
+}.isRequired;
 export default RecipeCard;
