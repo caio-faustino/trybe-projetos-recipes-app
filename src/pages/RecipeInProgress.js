1a151047d9fb93383d2846c88dbda4a62493db70
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-04-13 17:43:53 -0300

    Requisito 42

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import useSWR from 'swr';
-import * as Toggle from '@radix-ui/react-toggle';
 import { pegarReceita } from '../util/fetchers';
 import BtnShare from '../components/BtnShare';
 import BtnLike from '../components/BtnLike';
+import { useLocalStorage } from '../useLocalStorage';
 
 function RecipeInProgress() {
   const history = useHistory();
@@ -17,10 +17,27 @@ function RecipeInProgress() {
     () => pegarReceita(isMeal, id),
   );
 
-  console.log(data);
+  const [inProgressRecipes, setInProgressRecipes] = useLocalStorage(
+    'inProgressRecipes',
+    { meals: {}, drinks: {} },
+  );
+  if (!(id in inProgressRecipes[type])) {
+    const newProgressRecipe = { ...inProgressRecipes };
+    newProgressRecipe[type][id] = [];
+    setInProgressRecipes(newProgressRecipe);
+  }
+  console.log(inProgressRecipes);
+  console.log(inProgressRecipes[type][id]);
+
+  let contagemIngredientes = 0;
+
+  // console.log(data);
 
   return (
-    <div className="flex flex-col gap-[5px] items-center justify-between container h-full">
+    <div
+      className={ 'flex flex-col gap-[5px] items-center '
+        + 'justify-between container h-full' }
+    >
       {isLoading && (<div>Carregando receita...</div>)}
       {!isLoading && error && (
         <div>
@@ -48,38 +65,52 @@ function RecipeInProgress() {
             {data.strCategory}
           </p>
           <h2>Ingredientes</h2>
-          <ol>
-            {Object.keys(data).map((key) => {
-              if (!key.startsWith('strIngredient') || !data[key]) {
-                return null;
-              }
-              const index = key.split('strIngredient')[1];
-              return (
-                <div
-                  className="flex items-center mb-4"
-                  key={ index }
-                  data-testid={ `${index}-ingredient-step` }
+          {Object.keys(data).map((key) => {
+            if (!key.startsWith('strIngredient') || !data[key]) {
+              return null;
+            }
+            const index = key.split('strIngredient')[1];
+            contagemIngredientes = index;
+            return (
+              <div
+                className="flex items-center mb-4"
+                key={ index }
+                data-testid={ `${index}-ingredient-step` }
+              >
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  id={ `${index}-ingredient-step` }
+                  checked={ inProgressRecipes[type][id].includes(index) }
+                  onChange={ () => {
+                    const newProgressRecipe = { ...inProgressRecipes };
+                    if (newProgressRecipe[type][id].includes(index)) {
+                      newProgressRecipe[type][id] = newProgressRecipe[type][id]
+                        .filter((i) => i !== index);
+                    } else {
+                      newProgressRecipe[type][id].push(index);
+                    }
+                    setInProgressRecipes(newProgressRecipe);
+                  } }
+                />
+                <label
+                  htmlFor={ `${index}-ingredient-step` }
+                  className={ `label-ingrediente 
+                    ${inProgressRecipes[type][id].includes(index)
+                ? ' riscado' : ''}` }
                 >
-                  <input
-                    type="checkbox"
-                    className="checkbox"
-                  />
-                  <label
-                    htmlFor="default-checkbox"
-                    className="label-ingrediente"
-                  >
-                    {data[key]}
-                  </label>
-                </div>
-              );
-            })}
-          </ol>
+                  {data[key]}
+                </label>
+              </div>
+            );
+          })}
           <h2>Instruções</h2>
           <p data-testid="instructions">{data.strInstructions}</p>
           <button
             type="button"
             data-testid="finish-recipe-btn"
             className="botaoPadrao"
+            disabled={ inProgressRecipes[type][id].length < contagemIngredientes }
           >
             Finalizar
           </button>
